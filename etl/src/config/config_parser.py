aac3124e2853eafd00712157c613373f70d46400
cafalchio <mcafalchio@gmail.com>
2025-04-27 17:48:08 +0100

    feat: added db engine to config

--- etl/src/config/config_parser.py
+++ etl/src/config/config_parser.py
@@ -1,14 +1,18 @@
 # load config yaml and create a config object to be used elsewhere
-
+from sqlalchemy import create_engine
+import os
 import yaml
 
-CONFIG_FILE = "src/config/config.yaml"
+path = os.path.dirname(os.path.abspath(__file__))
+
+CONFIG_FILE = os.path.join(path, "config.yaml")
 
 class YamlPaserError(Exception):
     pass
 
 class YamlParser:
     config = None
+
     def __init__(self, file):
         with open(file, 'r') as f:
             self.config = yaml.safe_load(f)
@@ -20,7 +24,11 @@ class YamlParser:
             return self.config[attribute]
         except AttributeError:
             raise YamlPaserError(f"Could not get {attribute} from config")
-        
+    
+    @property
+    def db(self):
+        return create_engine(self.config.get("DATABASE"))
+    
 
 config = YamlParser(CONFIG_FILE)
 
