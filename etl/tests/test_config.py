3e22c458aeb633acbdb0d2331c2ce60310f47b08
cafalchio <mcafalchio@gmail.com>
2025-04-28 13:56:35 +0100

    feat: add test for key not found etl

--- etl/tests/test_config.py
+++ etl/tests/test_config.py
@@ -1,9 +1,21 @@
-from src.config.config_parser import YamlParser
+import pytest
 from unittest.mock import patch, mock_open
+from src.config.config_parser import KeyNotInConfigError, YamlParser
 
 
 def test_config():
-    with patch("src.config.config_parser.open", mock_open(read_data="TEST: config_test")):
-        config = YamlParser("") 
+    with patch(
+        "src.config.config_parser.open", mock_open(read_data="TEST: config_test")
+    ):
+        config = YamlParser("")
         expected = "config_test"
         assert config.get("TEST") == expected
+
+
+def test_config_key_not_found():
+    with patch(
+        "src.config.config_parser.open", mock_open(read_data="TEST: config_test")
+    ):
+        config = YamlParser("")
+        with pytest.raises(KeyNotInConfigError):
+            config.get("DummyKey")
